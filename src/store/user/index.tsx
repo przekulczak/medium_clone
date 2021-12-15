c2de691cca64203634cf4ef06776099600f3c180
Przemysław Kulczak <przekulczak@gmail.com>
2021-12-15 01:23:42 +0100

    dispose unnecesarry code

--- src/store/user/index.tsx
+++ src/store/user/index.tsx
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction, Action } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { UserDetails } from "types/user";
 
 export interface UserState {
